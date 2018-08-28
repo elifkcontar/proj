06b213d2c65e149fa1cd129788fab3bb4492c70b
elifkcontar <elifkcontar@gmail.com>
2018-08-28 14:12:30 +0200

    Add files via upload

---
+++ classification_task/classification_model.py
@@ -0,0 +1,127 @@
+import tensorflow as tf
+import keras.backend.tensorflow_backend
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.65)
+session = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+keras.backend.tensorflow_backend.set_session(session)
+
+from keras.layers import Input, Dense
+from keras.models import Model
+from data import main, generate_data
+from keras.models import model_from_json
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+#Read data
+train_id, train_label_c, valid_id, valid_label_c, test_id, test_label_c=main()
+
+train=generate_data(directory='/home/ekcontar/dat/', augmentation=True, shuffle=True, batch_size=10, file_list=train_id, label_1=train_label_c)
+
+validation=generate_data(directory='/home/ekcontar/dat/', augmentation=False, shuffle=True, batch_size=10, file_list=valid_id, label_1=valid_label_c)
+
+
+#Build the model
+img_height, img_width, img_channel=384,384,3
+input_tensor     = Input(shape=(img_height, img_width, img_channel))
+vgg_new_model = keras.applications.vgg16.VGG16(include_top=False, weights='imagenet', input_tensor=input_tensor, input_shape=(img_height, img_width, img_channel), pooling='avg')
+for layer in vgg_new_model.layers[:-5]:
+    layer.trainable = False
+result=Dense(100, activation='relu')(vgg_new_model.output)
+result=Dense(100, activation='relu')(result)
+result=Dense(1, activation='sigmoid', kernel_initializer='glorot_uniform')(result)
+model      = Model(inputs=vgg_new_model.input, outputs=result)
+
+#Check the model
+model.summary()
+
+opt=keras.optimizers.SGD(lr=0.001, momentum=0.9)
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy']
+)
+
+
+history=model.fit_generator(
+	    train,
+	    steps_per_epoch=40,
+	    epochs=200,
+	    validation_steps=10,
+	    validation_data=validation,
+	    class_weight={0:1.,
+                	1:5.}
+)
+
+#save model to JSON
+model_json = model.to_json()
+with open("cla.json", "w") as json_file:
+    json_file.write(model_json)
+#serialize weights to HDF5
+model.save_weights("cla.h5")
+print("Saved model to disk")
+
+
+#Accuracy and loss plot
+'''
+# summarize history for accuracy
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validation'], loc='upper left')
+plt.show()
+'''
+
+#Load model
+json_file = open('cla.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+#Load weights into new model
+loaded_model.load_weights("cla.h5")
+print("Loaded model from disk")
+
+for layer in loaded_model.layers:
+    layer.trainable = True
+
+#Check the model
+loaded_model.summary()
+
+
+opt=keras.optimizers.SGD(lr=0.0001, momentum=0.9)
+loaded_model.compile(
+    loss='binary_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy']
+)
+
+history=loaded_model.fit_generator(
+	    train,
+	    steps_per_epoch=40,
+	    epochs=60,
+	    validation_steps=10,
+	    validation_data=validation,
+	    class_weight={0:1.,
+                	1:5.}
+)
+
+#save model to JSON
+model_json = loaded_model.to_json()
+with open("class", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+loaded_model.save_weights("class.h5")
+print("Saved second model to disk")
+
+
---
+++ classification_task/evaluate.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import keras.backend.tensorflow_backend
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.15)
+session = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+keras.backend.tensorflow_backend.set_session(session)
+
+import numpy as np
+from data import main, generate_data
+from sklearn.metrics import roc_curve, auc, confusion_matrix
+from keras.models import model_from_json
+import matplotlib.pyplot as plt
+import itertools
+from scipy import stats
+
+#Read data
+train_id, train_label_c, valid_id, valid_label_c, test_id, test_label_c=main()
+
+test=generate_data(directory='/home/ekcontar/dat/', augmentation=False, shuffle=False, batch_size=10, file_list=test_id, label_1=test_label_c)
+
+#Load model
+json_file = open('class.json', 'r')
+model_json = json_file.read()
+json_file.close()
+load_model = model_from_json(model_json)
+#Load weights into new model
+load_model.load_weights("class.h5")
+print("Loaded model from disk")
+
+
+opt=keras.optimizers.SGD(lr=0.0001, momentum=0.9)
+load_model.compile(
+    loss='binary_crossentropy',
+    optimizer=opt,
+    metrics=['accuracy']
+)
+
+#Make prediction for class
+y_pred = load_model.predict_generator(test, steps=25)
+y_pred_c=np.array(y_pred)
+y_true=test_label_c
+
+#Confusion matrix
+classes={'nevus': 0, 'melanoma': 1}
+thre=0.5
+# obtain class predictions from probabilities
+y_predi=(y_pred_c>=thre)*1
+# obtain (unnormalized) confusion matrix
+cm = confusion_matrix(y_true, y_predi)
+# normalize confusion matrix
+cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+plt.figure()
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion matrix')
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+thresh = cm.max() / 2.
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+	plt.text(j, i, format(cm[i, j], '.2f'),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+plt.tight_layout()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
+#ROC curve and score
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+fpr, tpr, _ = roc_curve(y_true, y_pred)
+roc_auc = auc(fpr, tpr)
+plt.plot(fpr, tpr, color='aqua', lw=2,
+label='ROC curve  (area = {f:.2f})'.format( f=roc_auc))
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.show()
+
+
