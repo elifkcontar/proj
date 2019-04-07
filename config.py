a3715195e9590158e6db4f07bc6a989c7423ee01
Reuben Varghese <reubenvarghese1@gmail.com>
2019-04-08 04:57:59 +0530

    Project directory path error fixed

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 from os.path import dirname, abspath
 
 d = dirname(dirname(abspath(__file__)))
-dir_of_file = dirname(d)
-parent_dir_of_file = dirname(dir_of_file)
+d = d + '/'
+# dir_of_file = dirname(d)
+# parent_dir_of_file = dirname(dir_of_file)
 #parents_parent_dir_of_file = dirname(parent_dir_of_file)
 
 DATA_CONFIG = {
     'data_folder': '/home/reuben/newtest/',
-    'project_folder': parent_dir_of_file}
+    'project_folder': d}
